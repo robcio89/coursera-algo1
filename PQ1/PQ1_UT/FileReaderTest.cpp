3ff2caf79c11c8519a72d96fc73e6c071346501d
robcio89 <robcio89@gmail.com>
2015-05-13 09:44:00 +0200

    added UT project

---
+++ PQ1/PQ1_UT/FileReaderTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../PQ1/FileReader.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PQ1_UT
+{		
+	TEST_CLASS(FileReaderTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FileReader fr;
+		}
+
+	};
+}
